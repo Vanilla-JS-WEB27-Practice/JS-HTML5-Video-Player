c5d0f33398ed55498e460a09da994f0f087f677e
seanaleid <seanaleid@gmail.com>
2020-01-13 10:01:43 -0500

    Adds js code to select all the elements from html

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* Step 1: Get the elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip');
+const ranges = player.querySelector('.player__slider');
+
+/* Step 2: Build out the functions */
+
+/* Step 3: Hook up the event listeners */
