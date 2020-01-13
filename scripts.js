88172376c00fab079b2d44620e0309d5b4c68bfd
seanaleid <seanaleid@gmail.com>
2020-01-13 10:51:47 -0500

    Adds code to scrub the video, but still need to connect to the progress bar

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
+const progressBar = player.querySelector('.progressFilled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const ranges = player.querySelectorAll('.playerSlider');
 
 /* Step 2: Build out the functions */
 function togglePlay() {
@@ -40,13 +40,31 @@ function handleRangeUpdate() {
     console.log(this.value);
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.getElementsByClassName.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    console.log(e);
+    video.currentTime = scrubTime;
+}
+
 /* Step 3: Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 // ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousemove', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = true);
