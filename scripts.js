db277142bacb613af2b59922fa43bf047df120e4
seanaleid <seanaleid@gmail.com>
2020-01-13 10:20:20 -0500

    Adds functional code for skipButtons, play toggle, and updates play button content

--- scripts.js
+++ scripts.js
@@ -4,9 +4,40 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Step 2: Build out the functions */
+function togglePlay() {
+    // option 1: long way 
+    // if(video.paused){
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+
+    // option 2: short way
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    // console.log('Update the button');
+    console.log('icon');
+    toggle.textContent = icon;
+}
+
+function skip() {
+    // console.log('Skipping!');
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
 
 /* Step 3: Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
