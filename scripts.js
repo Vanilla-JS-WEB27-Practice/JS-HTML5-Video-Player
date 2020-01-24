22c0575542ca6e623367d4f2677cbb20f9e2b850
seanaleid <seanaleid@gmail.com>
2020-01-24 10:22:06 -0500

    Add functional code to scripts.js
      *Adds final code for the scrubber, the volume, the playback bar, and the progress bar

--- scripts.js
+++ scripts.js
@@ -42,20 +42,22 @@ function handleRangeUpdate() {
 
 function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100;
-    progressBar.getElementsByClassName.flexBasis = `${percent}%`;
+    progressBar.style.flexBasis = `${percent}%`;
 }
 
 function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     console.log(e);
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
 }
+
 
 /* Step 3: Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
@@ -66,5 +68,5 @@ ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 let mousedown = false;
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mousemove', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = true);
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
